a6813dcad7080d8472f2e8f8e2ed36a58081f2a8
Sebastiao Costa Jeronimo <scosta-j@c2r6s3.42lisboa.com>
2023-01-27 23:02:03 +0000

    made an recursive function for computing the size

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: scosta-j <scosta-j@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 19:05:51 by scosta-j          #+#    #+#             */
-/*   Updated: 2023/01/27 21:58:08 by scosta-j         ###   ########.fr       */
+/*   Updated: 2023/01/27 22:59:38 by scosta-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int		print_string(va_list args);
 int		print_signed_int(va_list args);
 int		print_unsigned(va_list args);
 int		print_hexadecimal(va_list args, char type);
-int		compute_size(unsigned long long int n, int base);
 int		turning_to_base(unsigned long int a, char *base, int base_len);
 int		print_pointer(va_list args);
+int		compute_size_rec(unsigned long long int n, int base);
 
 #endif
--- ft_printf_aux.c
+++ ft_printf_aux.c
@@ -6,7 +6,7 @@
 /*   By: scosta-j <scosta-j@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 21:47:47 by scosta-j          #+#    #+#             */
-/*   Updated: 2023/01/27 22:45:22 by scosta-j         ###   ########.fr       */
+/*   Updated: 2023/01/27 22:59:03 by scosta-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	turning_to_base(unsigned long int number, char *base, int base_len)
 	unsigned long long int	aux;
 	char					*hexa_s;
 
-	size = compute_size(number, base_len);
+	size = compute_size_rec(number, base_len);
 	aux = number;
 	hexa_s = (char *) malloc(size + 1);
 	hexa_s[size] = '\0';
@@ -45,20 +45,15 @@ int	turning_to_base(unsigned long int number, char *base, int base_len)
 		number = number / base_len;
 		size--;
 	}
-	size = write(1, hexa_s, compute_size(aux, base_len));
+	size = write(1, hexa_s, compute_size_rec(aux, base_len));
 	free(hexa_s);
 	return (size);
 }
 
-int	compute_size(unsigned long long int n, int base)
-{
-	int	size;
 
-	size = 0;
-	while (n > (unsigned long long int)(base - 1))
-	{
-	size++;
-	n = n / base;
-	}
-	return (size + 1);
+int	compute_size_rec(unsigned long long int n, int base)
+{
+	if (n > (unsigned long long int)(base - 1))
+		return (compute_size_rec(n / base, base) + 1);
+	return (1);
 }
